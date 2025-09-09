2b14814d7dd811347cc2d86496b6379fbc504d39
火山大隊長 <brian20020925@gmail.com>
2025-09-10 02:00:23 +0800

    fix: start path error

--- _start.sh
+++ _start.sh
@@ -7,6 +7,7 @@ set -x
 cd "/home/brian"
 git fetch origin
 git reset --hard origin/main
+cd "/home/brian/discord-bot"
 . "/home/brian/discord-bot/discord-bot/bin/activate"
 python3 "/home/brian/discord-bot/bot.py"
 
