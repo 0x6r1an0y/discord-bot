2420f3ea1120e946b066e9005d49dd028dd1ec37
火山大隊長 <brian20020925@gmail.com>
2025-09-10 01:25:28 +0800

    feat: check update

--- _start.sh
+++ _start.sh
@@ -4,6 +4,8 @@
 
 exec 2>error.log
 set -x
+cd "/home/brian"
+git clone https://github.com/0x6r1an0y/discord-bot.git
 . "/home/brian/discord-bot/discord-bot/bin/activate"
 python3 "/home/brian/discord-bot/bot.py"
 
